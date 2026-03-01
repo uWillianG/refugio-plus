f7d95f2c523b34aba12eed62fdaf723f182e0e1c
Willian Siqueira <willian.gabriel.siq@gmail.com>
2026-03-01 00:30:15 -0300

    config staticfiles

--- app/settings.py
+++ app/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'imagens/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'imagens')
